72bdb53f07e6e7ac5022a9eda600fbea57ab23ac
Hellowlol <hellowlol1@gmail.com>
2019-04-13 21:37:00 +0200

    add commands.

--- zaptec/sensor.py
+++ zaptec/sensor.py
@@ -158,7 +158,7 @@ class Charger(Entity):
     @property
     def icon(self):
         return 'mdi:ev-station'
-    
+
     @property
     def entity_picture(self):
         return CHARGE_MODE_MAP[self._attrs['charger_operation_mode']][1]
@@ -171,17 +171,114 @@ class Charger(Entity):
     def device_state_attributes(self):
         return self._attrs
 
-    async def stop_charging(self):
-        return await self._send_command(502)
+    # Add the commands i found but i assume some
+    # of them need some kind if input.
+    # untested..
 
     async def restart_charger(self):
         return await self._send_command(102)
 
+    async def restart_mcu(self):
+        return await self._send_command(103)
+
+    async def update_settings(self):
+        return await self._send_command(104)
+
+    async def restart_ntp(self):
+        return await self._send_command(105)
+
+    async def exit_app_with_code(self):
+        return await self._send_command(106)
+
     async def upgrade_firmware(self):
         return await self._send_command(200)
 
+    async def upgrade_firmware_forced(self):
+        return await self._send_command(201)
+
+    async def reset_com_errors(self):
+        return await self._send_command(260)
+
+    async def reset_notifications(self):
+        return await self._send_command(261)
+
+    async def reset_com_warnings(self):
+        return await self._send_command(262)
+
+    async def local_settings(self):
+        return await self._send_command(260)
+
+    async def set_plc_npw(self):
+        return await self._send_command(320)
+
+    async def set_plc_cocode(self):
+        return await self._send_command(321)
+
+    async def set_plc_nmk(self):
+        return await self._send_command(322)
+
+    async def set_remote_plc_nmk(self):
+        return await self._send_command(323)
+
+    async def set_remote_plc_npw(self):
+        return await self._send_command(324)
+
+    async def start_charging(self):
+        return await self._send_command(501)
+
+    async def stop_charging(self):
+        return await self._send_command(502)
+
+    async def report_charging_state(self):
+        return await self._send_command(503)
+
+    async def set_session_id(self):
+        return await self._send_command(504)
+
+    async def set_user_uuid(self):
+        return await self._send_command(505)
+
+    async def show_granted(self):
+        return await self._send_command(601)
+
+    async def show_denied(self):
+        return await self._send_command(602)
+
+    async def indicate_app_connect(self):
+        return await self._send_command(603)
+
+    async def confirm_charge_card_added(self):
+        return await self._send_command(750)
+
+    async def set_authentication_list(self):
+        return await self._send_command(751)
+
+    async def debug(self):
+        return await self._send_command(800)
+
+    async def get_plc_topology(self):
+        return await self._send_command(801)
+
+    async def reset_plc(self):
+        return await self._send_command(802)
+
+    async def remote_command(self):
+        return await self._send_command(803)
+
+    async def run_grid_test(self):
+        return await self._send_command(804)
+
+    async def run_post_production_test(self):
+        return await self._send_command(901)
+
+    async def combined_min(self):
+        return await self._send_command(10000)
+
     async def deauthorize_stop(self):
-        return await self._send_command(1001)
+        return await self._send_command(10001)
+
+    async def combined_max(self):
+        return await self._send_command(10999)
 
     async def _send_command(self, id_):
         await self.account._request('chargers/%s/SendCommand/%s' % (self._id, id_))
