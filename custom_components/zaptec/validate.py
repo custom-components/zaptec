41de8f5018daa6ebb12a5ce98667dae98a1c5d8c
steinmn <46349253+steinmn@users.noreply.github.com>
2025-08-22 19:26:20 +0200

    Migrate fully to pydantic 2 (#268)
    * Add Raises to validate docstring
    * Update requirements

--- custom_components/zaptec/validate.py
+++ custom_components/zaptec/validate.py
@@ -4,57 +4,66 @@ from __future__ import annotations
 
 import logging
 import re
+from typing import Any
 
-try:
-    from pydantic.v1 import BaseModel, ConfigDict, ValidationError
-except ImportError:
-    from pydantic import BaseModel, ConfigDict, ValidationError
+from pydantic import BaseModel, ConfigDict, TypeAdapter, ValidationError
 
 _LOGGER = logging.getLogger(__name__)
 
 
-class TypeWrapper:
-    """Workaround class for v1 pydantic."""
-
-
 class Installation(BaseModel):
+    """Pydantic model for a Zaptec installation."""
+
     model_config = ConfigDict(extra="allow")
     Id: str
+    Active: bool
+    AuthenticationType: int
+    CurrentUserRoles: int
+    InstallationType: int
+    NetworkType: int
 
 
 class Installations(BaseModel):
+    """Pydantic model for a list of Zaptec installations."""
+
     model_config = ConfigDict(extra="allow")
     Data: list[Installation]
+    Pages: int
 
 
 class Charger(BaseModel):
+    """Pydantic model for a Zaptec charger."""
+
     model_config = ConfigDict(extra="allow")
     Id: str
     Name: str
+    Active: bool
+    DeviceType: int
 
 
 class ChargerState(BaseModel):
+    """Pydantic model for a single state of a Zaptec charger."""
+
     model_config = ConfigDict(extra="allow")
     StateId: int
     ValueAsString: str
 
 
-# pydantic v2
-# ChargerStates = TypeAdapter[list[ChargerState]]
-class ChargerStates(TypeWrapper, BaseModel):
-    _data: list[ChargerState]
-
-
-class ChargerUpdate(TypeWrapper, BaseModel):
-    _data: dict[str, str]
+ChargerStates = TypeAdapter[list[ChargerState]]
+ChargerUpdate = TypeAdapter[dict[str, str]]
 
 
 class Chargers(BaseModel):
+    """Pydantic model for a list of Zaptec chargers."""
+
     model_config = ConfigDict(extra="allow")
     Data: list[Charger]
+    Pages: int
 
 
 class Circuit(BaseModel):
+    """Pydantic model for a Zaptec circuit."""
+
     model_config = ConfigDict(extra="allow")
     Id: str
     Name: str
@@ -62,40 +71,54 @@ class Circuit(BaseModel):
 
 
 class Hierarchy(BaseModel):
+    """Pydantic model for the hierarchy of Zaptec objects in an installation."""
+
     model_config = ConfigDict(extra="allow")
     Id: str
     Name: str
+    NetworkType: int
     Circuits: list[Circuit]
 
 
 class ChargerFirmware(BaseModel):
+    """Pydantic model for the firmware information of a Zaptec charger."""
+
     model_config = ConfigDict(extra="allow")
     ChargerId: str
+    DeviceType: int
+    IsOnline: bool
     CurrentVersion: str
     AvailableVersion: str
     IsUpToDate: bool
 
 
 class ChargerLocalSettings(BaseModel):
+    """
+    Pydantic model for the local settings of a Zaptec charger.
+
+    Note: This model is used in an undocumented API-call, and should be removed as soon as we have
+    official API-calls that can cover the necessary functionality
+    """
+
     model_config = ConfigDict(extra="allow")
     Id: str
-    # Name: str
-    # DeviceId: str
+    Name: str | None = None
+    DeviceId: str | None = None
 
 
-# pydantic v2
-# ChargerFirmwares = TypeAdapter[list[ChargerFirmware]]
-class ChargerFirmwares(TypeWrapper, BaseModel):
-    _data: list[ChargerFirmware]
+ChargerFirmwares = TypeAdapter[list[ChargerFirmware]]
 
 
 class InstallationConnectionDetails(BaseModel):
+    """Pydantic model for the servicebus connection details of a Zaptec installation."""
+
     model_config = ConfigDict(extra="allow")
     Host: str
     Password: str
-    # Port: int
+    Port: int
+    UseSSL: bool
     Subscription: str
-    # Type: int
+    Type: int
     Username: str
     Topic: str
 
@@ -121,29 +144,24 @@ URLS = {
 _URLS = [(k, re.compile(k), v) for k, v in URLS.items()]
 
 
-def validate(data, url):
-    """Validate the data."""
+def validate(data: Any, url: str) -> None:
+    """
+    Validate the data.
+
+    Raises:
+        ValidationError: If data doesn't match the pydantic model associated with the url.
+
+    """
 
     for pat, re_pat, model in _URLS:
         # Mathes either the exact string or its regexp
         if url == pat or re_pat.fullmatch(url):
             try:
-                d = data
-
-                # pydantic v1
-                if isinstance(model, TypeWrapper):
-                    d = {"_data": data}
-
                 if isinstance(model, BaseModel):
-                    # pydantic v1
-                    model.parse_obj(d)
+                    model.model_validate(data, strict=True)
 
-                    # pydantic v2
-                    # model.model_validate(data, strict=True)
-
-                # pydantic v2
-                # elif isinstance(model, TypeAdapter):
-                #     model.validate_python(data, strict=True)
+                elif isinstance(model, TypeAdapter):
+                    model.validate_python(data, strict=True)
 
             except ValidationError as err:
                 _LOGGER.error("Failed to validate %s (pattern %s): %s", url, pat, err)
