e67a923232326d43ce722efad9074a6e4b0ed09c
Stein M. Nornes <stein@nlink.no>
2025-09-25 22:06:34 +0200

    Move old test code from diagnostics to pytest

--- tests/zaptec/test_diagnostics.py
+++ tests/zaptec/test_diagnostics.py
@@ -1,11 +1,14 @@
-"""Tests for zaptec/api.py."""
+"""Tests for diagnostics.py."""
 
 import asyncio
+from dataclasses import dataclass
 import logging
 import os
 
+import aiohttp
 import pytest
 
+from custom_components.zaptec.diagnostics import _get_diagnostics
 from custom_components.zaptec.zaptec.api import Zaptec
 
 # Always set to "true" when GitHub Actions is running a workflow
@@ -19,9 +22,9 @@ _LOGGER = logging.getLogger(__name__)
 
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 @pytest.mark.skipif(SKIP_API_TEST, reason="User disabled the tests requiring API login.")
-def test_api() -> None:
+def test_diagnostics() -> None:
     """
-    Test the Zaptec API.
+    Test the Zaptec Integration diagnostics.
 
     Does not run when testing in Github actions, since it requires login credentials.
     """
@@ -42,18 +45,35 @@ def test_api() -> None:
     logging.getLogger("azure").setLevel(logging.WARNING)
 
     async def gogo() -> None:
-        async with Zaptec(username, password) as zaptec:
-            # Builds the interface.
-            await zaptec.login()
+        async with (
+            aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session,
+            Zaptec(username, password, client=session) as zaptec,
+        ):
             await zaptec.build()
-            await zaptec.poll(info=True, state=True, firmware=True)
 
-            # Dump redaction database
-            _LOGGER.info("Redaction database:")
-            _LOGGER.info(zaptec.redact.dumps())
+            #
+            # Mocking to pretend to be a hass instance
+            # NOTE: This fails on getting entity lists in the output, but that's
+            # ok for this test.
+            #
+            @dataclass
+            class FakeZaptecManager:
+                zaptec: Zaptec
 
-            # Print all the attributes.
-            for obj in zaptec.objects():
-                _LOGGER.info(obj.asdict())
+            @dataclass
+            class FakeConfig:
+                runtime_data: FakeZaptecManager
+
+            manager = FakeZaptecManager(
+                zaptec=zaptec,
+            )
+            config = FakeConfig(
+                runtime_data=manager,
+            )
+            hass = None
+
+            # Get the diagnostics info
+            out = await _get_diagnostics(hass, config)
+            _LOGGER.info(out)
 
     asyncio.run(gogo())
