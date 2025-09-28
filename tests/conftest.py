89a5cc50ba7e94acddd59da98cf4675b59a80b67
steinmn <46349253+steinmn@users.noreply.github.com>
2025-09-28 20:47:30 +0200

    Move old API test code into pytest framework (#327)
    * disable it on Github actions since it requires login credentials.
    * Move old test code from diagnostics to pytest
    * Use pytest-asyncio
    * Rewrite global variables to fixtures in conftest.py
    * Move test_diagnostics.py to keep API- and HA-tests separate

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,51 @@
 """Zaptec testing configuration file."""
+
+import os
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def skip_if_in_github_actions() -> None:
+    """Check if we are running in Github actions and skip any dependant tests if true."""
+    if os.getenv("GITHUB_ACTIONS") == "true":
+        pytest.skip("This test doesn't work in Github Actions.")
+
+
+@pytest.fixture(scope="session")
+def skip_if_user_disabled_api_tests() -> None:
+    """Check if user has disabled API tests and skip any dependant tests if true."""
+    if os.getenv("SKIP_ZAPTEC_API_TEST") == "true":
+        pytest.skip("User disabled the tests requiring API login.")
+
+
+@pytest.fixture(scope="session")
+def zaptec_username(skip_if_user_disabled_api_tests, skip_if_in_github_actions) -> str:  # noqa: ANN001, ARG001 (the inputs are purely to create dependencies to the env-flags above)
+    """
+    Get the zaptec username stored in env.
+
+    Any test relying on this fixture will be skipped if the test is running
+    in Gihub Actions, or the user has disabled tests requiring API login.
+    """
+    username = os.environ.get("ZAPTEC_USERNAME")
+    assert username, (
+        "Missing username, either set it with \"export ZAPTEC_USERNAME='username'\" "
+        "or run test script with the --skip-api flag"
+    )
+    return username
+
+
+@pytest.fixture(scope="session")
+def zaptec_password(skip_if_user_disabled_api_tests, skip_if_in_github_actions) -> str:  # noqa: ANN001, ARG001
+    """
+    Get the zaptec password stored in env.
+
+    Any test relying on this fixture will be skipped if the test is running
+    in Gihub Actions, or the user has disabled tests requiring API login.
+    """
+    password = os.environ.get("ZAPTEC_PASSWORD")
+    assert password, (
+        "Missing password, either set it with \"export ZAPTEC_PASSWORD='password'\" "
+        "or run test script with the --skip-api flag"
+    )
+    return password
