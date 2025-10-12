11076ea3c790d7cc09d8844e401cdfee014ca1ba
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2025-10-12 23:11:44 +0200

    Skip attribute initialization when firmware info is missing (#324)
    * Add warning for missing firmware info in charger attributes, prevents the integration from crashing while starting.
    * This only happens for chargers that are added to the Zaptec portal, but not configured or correctly added.

--- custom_components/zaptec/zaptec/api.py
+++ custom_components/zaptec/zaptec/api.py
@@ -317,6 +317,19 @@ class Installation(ZaptecBase):
                 charger = self.zaptec.get(fm["ChargerId"])
                 if charger is None:
                     continue
+                if (
+                    fm.get("CurrentVersion") is None
+                    or fm.get("AvailableVersion") is None
+                    or fm.get("IsUpToDate") is None
+                ):
+                    # If the charger is already added to the Zaptec platform but not yet
+                    # initialized, these fields are not available.
+                    _LOGGER.warning(
+                        "Missing firmware info for charger %s because the charger hasn't been initialized yet. Safe to ignore.",  # noqa: E501
+                        charger.qual_id,
+                    )
+                    continue
+
                 charger.set_attributes(
                     {
                         "firmware_current_version": fm["CurrentVersion"],
